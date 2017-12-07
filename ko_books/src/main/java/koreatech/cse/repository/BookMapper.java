28edccf51b4361dab571ab89029b12dbb7e914d0
nemoland0506 <nemoland0506@gmail.com>
2017-12-07 22:27:45 +0900

    success deploy project in aws

--- ko_books/src/main/java/koreatech/cse/repository/BookMapper.java
+++ ko_books/src/main/java/koreatech/cse/repository/BookMapper.java
@@ -14,11 +14,11 @@ import java.util.List;
 
 @Repository
 public interface BookMapper {
-    @Insert("INSERT INTO BOOKS (TITLE, AUTHOR, PUBLISHER, PUBDATE, MAJOR, USERID, PHONE, PRICE) VALUES (#{title}, #{author}, #{publisher}, #{pubdate}, #{major}, #{userId}, #{phone}, #{price})")
+    @Insert("INSERT INTO books (TITLE, AUTHOR, PUBLISHER, PUBDATE, MAJOR, USERID, PHONE, PRICE) VALUES (#{title}, #{author}, #{publisher}, #{pubdate}, #{major}, #{userId}, #{phone}, #{price})")
     @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id", before = false, resultType = int.class)
     void insert(Book book);
 
-    @Delete("DELETE FROM BOOKS WHERE ID = #{id}")
+    @Delete("DELETE FROM books WHERE ID = #{id}")
     void delete(@Param("id") int id);
 
     @SelectProvider(type = BookSqlProvider.class, method = "findAllByProvider")
@@ -26,7 +26,7 @@ public interface BookMapper {
 
     //@formatter off
     @Select("<script>"
-            + "SELECT * FROM BOOKS"
+            + "SELECT * FROM books"
             + "<if test='title != null'> WHERE NAME = #{title}</if>"
             + "<if test='title != null and author != null'> OR AUTHOR = #{author}</if>"
             + "<if test='order != null'>ORDER BY ${order} DESC</if>"
